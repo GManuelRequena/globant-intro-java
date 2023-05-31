9f45fdefd6251ac141ab4d4ff2e3bcc81ae6b78c
Manuel Requena <48953482+GManuelRequena@users.noreply.github.com>
2023-05-31 20:42:33 -0300

    Exercise 1 complete

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Exercise1 ex1 = new Exercise1();
+        ex1.Suma();
     }
 }
