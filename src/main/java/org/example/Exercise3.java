4dc04d956710f14bfe9e6f26c7739276f9eb2cd2
Manuel Requena <48953482+GManuelRequena@users.noreply.github.com>
2023-05-31 20:54:59 -0300

    Exercise 3 Complete

---
+++ src/main/java/org/example/Exercise3.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Exercise3 {
+    /*
+    Escribir un programa que pida una frase y la muestre toda en mayúsculas y después toda
+    en minúsculas. Nota: investigar la función toUpperCase() y toLowerCase() en Java.
+     */
+
+    Scanner keyboard = new Scanner(System.in);
+
+    public void ChangeUpperLowerCase(){
+        System.out.println("Write a sentence: ");
+        String sentence = keyboard.nextLine();
+
+        System.out.println("The sentence in UpperCase is: " + sentence.toUpperCase());
+        System.out.println("The sentence in LowerCase is: " + sentence.toLowerCase());
+    }
+}
