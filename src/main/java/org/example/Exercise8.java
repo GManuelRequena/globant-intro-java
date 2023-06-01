5664ac718d80f9130d4c76bdb19bd82151b5e9b0
Manuel Requena <48953482+GManuelRequena@users.noreply.github.com>
2023-05-31 21:42:36 -0300

    Exerse 8 Complete

---
+++ src/main/java/org/example/Exercise8.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Exercise8 {
+    /*
+    Realizar un programa que solo permita introducir solo frases o palabras de 8 de largo. Si el
+    usuario ingresa una frase o palabra de 8 de largo se deberá de imprimir un mensaje por
+    pantalla que diga "CORRECTO", en caso contrario, se deberá imprimir "INCORRECTO".
+    Nota: investigar la función Lenght() en Java.
+     */
+
+    Scanner keyboard = new Scanner(System.in);
+
+    public void PhraseLenght(){
+        System.out.print("Enter a phrase: ");
+        String phrase = keyboard.nextLine();
+
+        if(phrase.length() <= 8){
+            System.out.println("CORRECT");
+        }else {
+            System.out.println("INCORRECT");
+        }
+    }
+}
