06f1ab1cd01e3239ca0379e9003a8a1672a0dba3
Manuel Requena <48953482+GManuelRequena@users.noreply.github.com>
2023-06-01 19:46:37 -0300

    Exercise 10 Complete

---
+++ src/main/java/org/example/Exercise10.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Exercise10 {
+    /*
+    Escriba un programa en el cual se ingrese un valor límite positivo, y a continuación solicite
+    números al usuario hasta que la suma de los números introducidos supere el límite inicial.
+     */
+    Scanner keyboard = new Scanner(System.in);
+    public void SumaLimite(){
+        Integer numero = 0;
+        Integer limite = 0;
+        Integer suma = 0;
+
+        System.out.print("Ingrese un limite positivo: ");
+        limite = keyboard.nextInt();
+
+        while(suma <= limite){
+            System.out.println("Ingrese un numero: ");
+            suma += keyboard.nextInt();
+        }
+    }
+}
