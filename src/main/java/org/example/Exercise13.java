ea19b2fc3b624c63fddba217dce2ebd8cf096455
Manuel Requena <48953482+GManuelRequena@users.noreply.github.com>
2023-06-02 14:26:43 -0300

    Exercise 13 Complete

---
+++ src/main/java/org/example/Exercise13.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Exercise13 {
+    /*
+    Dibujar un cuadrado de N elementos por lado utilizando el carácter "*". Por ejemplo, si el
+    cuadrado tiene 4 elementos por lado se deberá dibujar lo siguiente:
+    * * * *
+    *     *
+    *     *
+    * * * *
+
+     */
+
+    Scanner keyboard = new Scanner(System.in);
+
+    public void DibujarCuadrado(){
+        System.out.print("Ingrese cantidad de *: ");
+        int cant = keyboard.nextInt();
+
+        for(int i = 0; i < cant; i++){
+            for(int j = 0; j < cant; j++){
+                if(i == 0 || i == cant-1 || j == 0 || j == cant-1){
+                    System.out.print("* ");
+                }else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
